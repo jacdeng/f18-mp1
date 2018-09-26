ff8bd0c2554f2b2168fc816c54ddae401aae3788
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2018-09-25 21:37:28 -0700

    updated public tests

--- src/test/java/mp1/MP1TestPublic.java
+++ src/test/java/mp1/MP1TestPublic.java
@@ -1,6 +1,5 @@
 package mp1;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -30,8 +29,8 @@ public class MP1TestPublic {
     public static void setup() throws IOException {
         docList = new ArrayList<Document>();
 
-//        studyInScarlet = new Document("Conan Doyle: A Study in Scarlet", "http://www.gutenberg.org/files/244/244-0.txt");
-//        docList.add(studyInScarlet);
+        studyInScarlet = new Document("Conan Doyle: A Study in Scarlet", "http://www.gutenberg.org/files/244/244-0.txt");
+        docList.add(studyInScarlet);
 
         blackPeter = new Document("Conan Doyle: Adventure of Black Peter", "http://textfiles.com/stories/blackp.txt");
         docList.add(blackPeter);
@@ -39,8 +38,8 @@ public class MP1TestPublic {
         tempest = new Document("Shakespeare: The Tempest", "http://www.gutenberg.org/files/23042/23042-0.txt");
         docList.add(tempest);
 
-//        persuasion = new Document("Austen: Persuasion", "http://www.gutenberg.org/cache/epub/105/pg105.txt");
-//        docList.add(persuasion);
+        persuasion = new Document("Austen: Persuasion", "http://www.gutenberg.org/cache/epub/105/pg105.txt");
+        docList.add(persuasion);
 
         prideAndPrejudice = new Document("Austen: Pride and Prejudice", "http://www.gutenberg.org/files/1342/1342-0.txt");
         docList.add(prideAndPrejudice);
@@ -48,14 +47,14 @@ public class MP1TestPublic {
         arcticCircle = new Document("Prentice: The Arctic Circle and Beyond", "http://textfiles.com/stories/arctic.txt");
         docList.add(arcticCircle);
 
-//        flossieFumble = new Document("Flossie Fumble: Letter to Blossom", "http://textfiles.com/stories/blossom.pom");
-//        docList.add(flossieFumble);
+        flossieFumble = new Document("Flossie Fumble: Letter to Blossom", "http://textfiles.com/stories/blossom.pom");
+        docList.add(flossieFumble);
 
         aesop = new Document("Aesop: Fables", "http://textfiles.com/stories/aesop11.txt");
         docList.add(aesop);
 
-//        lastFreeMan = new Document("Porretto: The Last Free Man", "http://textfiles.com/stories/freeman.fil");
-//        docList.add(lastFreeMan);
+        lastFreeMan = new Document("Porretto: The Last Free Man", "http://textfiles.com/stories/freeman.fil");
+        docList.add(lastFreeMan);
 
         immortality = new Document("Burroughs: Immortality", "http://textfiles.com/stories/immortal");
         docList.add(immortality);
@@ -81,7 +80,7 @@ public class MP1TestPublic {
 
     @Test
     public void testSimilarity1() {
-        assertEquals(new DocumentPair(prideAndPrejudice, studyInScarlet), DocumentSimilarity.closestMatch(docList));
+        assertEquals(new DocumentPair(prideAndPrejudice, persuasion), DocumentSimilarity.closestMatch(docList));
     }
 
     @Test
@@ -90,22 +89,36 @@ public class MP1TestPublic {
     }
 
     @Test
+    public void testSentiment2() {
+        assertEquals(20, studyInScarlet.getOverallSentiment());
+    }
+
+    @Test
+    public void testSentiment3() {
+        assertEquals(9, lastFreeMan.getOverallSentiment());
+    }
+
+    @Test
     public void testSentimentDiff1() {
         DocumentPair docPair = DocumentSimilarity.sentimentDiffMax(docList);
-        DocumentPair expPair = new DocumentPair(studyInScarlet, prideAndPrejudice);
+        DocumentPair expPair = new DocumentPair(lastFreeMan, prideAndPrejudice);
         assertEquals(expPair, docPair);
     }
 
     @Test
     public void testGrouping1() {
         HashMap<Document, Document> expMap = new HashMap<>();
+        expMap.put(tempest, aesop);
+        expMap.put(flossieFumble, flossieFumble);
+        expMap.put(lastFreeMan, lastFreeMan);
+        expMap.put(studyInScarlet, aesop);
+        expMap.put(persuasion, aesop);
+        expMap.put(arcticCircle, aesop);
         expMap.put(blackPeter, aesop);
-        expMap.put(tempest, tempest);
-        expMap.put(prideAndPrejudice, aesop);
-        expMap.put(arcticCircle, arcticCircle);
         expMap.put(aesop, aesop);
-        expMap.put(immortality, immortality);
+        expMap.put(prideAndPrejudice, aesop);
         expMap.put(sixNapoleons, aesop);
+        expMap.put(immortality, immortality);
         assertEquals(expMap, DocumentSimilarity.groupSimilarDocuments(docList, 4));
     }
 
